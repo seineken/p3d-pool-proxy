80e87b936362750e5941304563ae495195ae42b6
Roberto Chajin Ortiz <robertochajin@gmail.com>
2023-09-30 15:01:18 -0400

    v0.1.0

--- src/pool_rpc.rs
+++ src/pool_rpc.rs
@@ -1,246 +1,74 @@
-use std::collections::HashSet;
 use std::sync::Arc;
 
-use crate::rpc::{AlgoType, MiningObj, MiningProposal, P3dParams, Payload};
-use crate::worker::{Compute, DoubleHash};
-use codec::{Codec, Encode};
-use ecies_ed25519::encrypt;
-use jsonrpsee::core::client::ClientT;
-use jsonrpsee::core::JsonValue;
-use jsonrpsee::core::{async_trait, Error as JsonRpseeError, RpcResult};
-use jsonrpsee::http_client::{HttpClient, HttpClientBuilder};
+use crate::rpc::PoolContex;
+use jsonrpsee::core::{async_trait, RpcResult};
 use jsonrpsee::proc_macros::rpc;
-use jsonrpsee::rpc_params;
-use p3d::p3d_process;
-use primitive_types::{H256, U256};
-use rand::{rngs::StdRng, SeedableRng};
-use schnorrkel::{ExpansionMode, MiniSecretKey, PublicKey, SecretKey, Signature};
-use std::str::FromStr;
-
-fn sign(key: SecretKey, msg: &[u8]) -> Signature {
-    const CTX: &[u8] = b"Mining pool";
-    key.sign_simple(CTX, msg, &key.to_public())
-}
-
-pub fn get_hash_difficulty(hash: &H256) -> U256 {
-    let num_hash = U256::from(&hash[..]);
-    let max = U256::max_value();
-    max / num_hash
-}
-
-type ParamsResp = (H256, H256, U256, U256, U256);
 
 #[rpc(server, client)]
 pub trait MiningRpc {
+    /// get_meta ask to the blockchain for SOLO mining params
     #[method(name = "get_meta")]
     async fn get_meta(&self) -> RpcResult<String>;
 
+    /// get_mining_params ask to the blockchain for POOL mining params
     #[method(name = "get_mining_params")]
     async fn get_mining_params(&self) -> RpcResult<String>;
 
+    /// push_to_node handles the payload from the miner and push it to the node
+    #[method(name = "push_to_node")]
+    async fn push_to_node(&self, hash: String, obj: String) -> RpcResult<String>;
+
+    /// push_to_pool handles the payload from the miner and push it to the POOL
     #[method(name = "push_to_pool")]
-    async fn push_to_pool(
-        &self,
-        pool_id: String,
-        member_id: String,
-        pre_hash: String,
-        parent_hash: String,
-        win_difficulty: String,
-        pow_difficulty: String,
-        hash: String,
-        obj: String,
-        pub_key: String,
-    ) -> RpcResult<u64>;
+    async fn push_to_pool(&self, hash: String, obj: String) -> RpcResult<String>;
 }
 
 pub struct MiningRpcServerImpl {
-    pub(crate) p3d_params: P3dParams,
-    pub(crate) pool_id: String,
-    pub(crate) member_id: String,
-    pub(crate) algo: String,
-    pub(crate) key: SecretKey,
-    pub(crate) client: Arc<HttpClient>,
+    pub(crate) ctx: Arc<PoolContex>,
 }
 
 impl MiningRpcServerImpl {
-    pub fn new(
-        p3d_params: P3dParams,
-        pool_id: String,
-        member_id: String,
-        algo: String,
-        key: SecretKey,
-        client: Arc<HttpClient>,
-    ) -> Self {
-        Self {
-            p3d_params,
-            pool_id,
-            member_id,
-            algo,
-            key,
-            client,
-        }
+    pub fn new(ctx: Arc<PoolContex>) -> Self {
+        Self { ctx }
     }
 }
 
 #[async_trait]
 impl MiningRpcServer for MiningRpcServerImpl {
     async fn get_meta(&self) -> RpcResult<String> {
-        let meta = self
-            .client
-            .request::<String, _>("poscan_getMeta", rpc_params![])
+        let response = self
+            .ctx
+            .get_meta()
             .await
+            .map_err(|e| e.to_string())
             .unwrap();
-        Ok(meta)
+        Ok(response)
     }
     async fn get_mining_params(&self) -> RpcResult<String> {
-        let meta: JsonValue = self
-            .client
-            .request::<JsonValue, _>(
-                "poscan_getMiningParams",
-                rpc_params![serde_json::json!(self.pool_id)],
-            )
+        let response = self
+            .ctx
+            .get_mining_params()
             .await
+            .map_err(|e| e.to_string())
             .unwrap();
-
-        let mut content: Vec<String> = Vec::new();
-
-        if let Some(params) = meta.as_array() {
-            for param in params {
-                if let Some(param_str) = param.as_str() {
-                    // println!("{}", param_str);
-                    content.push(param_str.to_string());
-                }
-            }
-        }
-
-        let pre_hash = H256::from_str(&content[0].clone()).unwrap();
-        let parent_hash = H256::from_str(&content[1].clone()).unwrap();
-        let win_difficulty = U256::from_str_radix(&content[2].clone(), 16).unwrap();
-        let pow_difficulty = U256::from_str_radix(&content[3].clone(), 16).unwrap();
-        let pub_key = U256::from_str_radix(&content[4].clone(), 16).unwrap();
-
-        Ok(format!(
-            "{}",
-            hex::encode(
-                (
-                    pre_hash,
-                    parent_hash,
-                    win_difficulty,
-                    pow_difficulty,
-                    pub_key,
-                )
-                    .encode()
-            )
-        ))
+        Ok(response)
     }
-    async fn push_to_pool(
-        &self,
-        pool_id: String,
-        member_id: String,
-        pre_hash: String,
-        parent_hash: String,
-        win_difficulty: String,
-        pow_difficulty: String,
-        hash: String,
-        obj: String,
-        pub_key: String,
-    ) -> RpcResult<u64> {
-        let P3dParams { algo, sect, grid } = self.p3d_params.clone();
-        let mut processed_hashes: HashSet<H256> = HashSet::new();
-
-        let hash = H256::from_str(hash.as_str()).unwrap();
-
-        let mut payload = Payload {
-            pool_id: self.pool_id.clone(),
-            member_id: self.member_id.clone(),
-            pre_hash: H256::from_str(pre_hash.as_str()).unwrap(),
-            parent_hash: H256::from_str(parent_hash.as_str()).unwrap(),
-            algo: self.algo.clone(),
-            dfclty: U256::from_str_radix(pow_difficulty.as_str(), 16).unwrap(),
-            hash,
-            obj_id: 1,
-            obj: obj.as_bytes().to_vec(),
-        };
-
-        let rot_hash = match &algo {
-            AlgoType::Grid2dV3_1 => payload.pre_hash.clone(),
-            _ => payload.parent_hash.clone(),
-        };
-
-        let rot = rot_hash.encode()[0..4].try_into().ok();
-
-        let mining_obj: MiningObj = MiningObj {
-            obj_id: 1,
-            obj: payload.obj.clone(),
-        };
-
-        let res_hashes = p3d_process(
-            mining_obj.obj.as_slice(),
-            algo.as_p3d_algo(),
-            grid as i16,
-            sect as i16,
-            rot,
-        );
-
-        let (first_hash, obj_hash, poscan_hash) = match res_hashes {
-            Ok(hashes) if !hashes.is_empty() => {
-                let first_hash = hashes[0].clone();
-                let obj_hash = H256::from_str(&first_hash).unwrap();
-                // if processed_hashes.contains(&obj_hash) {
-                //     continue;
-                // }
-                let poscan_hash = DoubleHash {
-                    pre_hash: payload.pre_hash.clone(),
-                    obj_hash,
-                }
-                .calc_hash();
-                processed_hashes.insert(obj_hash.clone());
-                (first_hash, obj_hash, poscan_hash)
-            }
-            _ => panic!("Something really weird is going on in the Antartida."),
-        };
-
-        for difficulty in [
-            payload.dfclty.clone(),
-            U256::from_str_radix(&win_difficulty, 16).unwrap(),
-        ] {
-            let comp = Compute {
-                difficulty,
-                pre_hash: payload.pre_hash.clone(),
-                poscan_hash,
-            };
-
-            let diff = get_hash_difficulty(&comp.get_work());
-
-            if diff >= difficulty {
-                payload.hash = obj_hash;
-
-                let pub_key = U256::from_str_radix(&pub_key, 16).unwrap();
-                let mut pub_key = pub_key.encode();
-                pub_key.reverse();
-                let pub_key = ecies_ed25519::PublicKey::from_bytes(&pub_key).unwrap();
-
-                let message = serde_json::to_string(&payload).unwrap();
-                let mut csprng = StdRng::from_seed(obj_hash.encode().try_into().unwrap());
-                let encrypted = encrypt(&pub_key, message.as_bytes(), &mut csprng).unwrap();
-                let sign = sign(self.key.clone(), &encrypted);
-
-                let params = rpc_params![
-                    serde_json::json!(encrypted.clone()),
-                    serde_json::json!(self.member_id.clone()),
-                    serde_json::json!(hex::encode(sign.to_bytes()))
-                ];
-
-                let _response: JsonValue = self
-                    .client
-                    .request("poscan_pushMiningObjectToPool", params)
-                    .await
-                    .unwrap();
-
-                println!("{}", _response);
-            }
-        }
-
-        Ok(0)
+    async fn push_to_node(&self, hash: String, obj: String) -> RpcResult<String> {
+        let response = self
+            .ctx
+            .push_to_node(hash, obj)
+            .await
+            .map_err(|e| e.to_string())
+            .unwrap();
+        Ok(response)
+    }
+    async fn push_to_pool(&self, hash: String, obj: String) -> RpcResult<String> {
+        let response = self
+            .ctx
+            .push_to_pool(hash, obj)
+            .await
+            .map_err(|e| e.to_string())
+            .unwrap();
+        Ok(response)
     }
 }
--- src/worker.rs
+++ src/worker.rs
@@ -1,144 +1,12 @@
-use codec::Encode;
 use hyper::Method;
-use jsonrpsee::core::JsonValue;
 use jsonrpsee::server::{RpcModule, Server};
-use p3d::p3d_process;
-use primitive_types::{H256, U256};
-use sha3::{Digest, Sha3_256};
-use std::collections::HashSet;
 use std::net::SocketAddr;
-use std::str::FromStr;
 use std::sync::Arc;
-use std::time::Duration;
-use tokio::time;
 use tower_http::cors::{Any, CorsLayer};
 
 use crate::pool_rpc::{MiningRpcServer, MiningRpcServerImpl};
 
-use crate::rpc::{AlgoType, MiningObj, MiningParams, MiningProposal, P3dParams};
-
 use super::PoolContex;
-
-const ASK_MINING_PARAMS_PERIOD: Duration = Duration::from_secs(1);
-
-#[derive(Encode)]
-pub struct DoubleHash {
-    pub pre_hash: H256,
-    pub obj_hash: H256,
-}
-
-impl DoubleHash {
-    pub fn calc_hash(self) -> H256 {
-        H256::from_slice(Sha3_256::digest(&self.encode()[..]).as_slice())
-    }
-}
-
-#[derive(Clone, Encode)]
-pub struct Compute {
-    pub difficulty: U256,
-    pub pre_hash: H256,
-    pub poscan_hash: H256,
-}
-
-impl Compute {
-    pub(crate) fn get_work(&self) -> H256 {
-        let encoded_data = self.encode();
-        let hash_digest = Sha3_256::digest(&encoded_data);
-        H256::from_slice(&hash_digest)
-    }
-}
-
-pub fn get_hash_difficulty(hash: &H256) -> U256 {
-    let num_hash = U256::from(&hash[..]);
-    let max = U256::max_value();
-    max / num_hash
-}
-
-pub(crate) async fn queue_management(ctx: Arc<PoolContex>) {
-    let P3dParams { algo, sect, grid } = ctx.p3d_params.clone();
-    let mut processed_hashes: HashSet<H256> = HashSet::new();
-
-    loop {
-        let maybe_prop = {
-            let mut lock = ctx.out_queue.lock().unwrap();
-            (*lock).pop_front()
-        };
-        if let Some(prop) = maybe_prop {
-            println!("Entrando: {:?}", prop.hash.clone());
-            loop {
-                let rot_hash = match &algo {
-                    AlgoType::Grid2dV3_1 => prop.params.pre_hash.clone(),
-                    _ => prop.params.parent_hash.clone(),
-                };
-
-                let rot = rot_hash.encode()[0..4].try_into().ok();
-
-                let mining_obj: MiningObj = MiningObj {
-                    obj_id: 1,
-                    obj: prop.obj.clone(),
-                };
-
-                let res_hashes = p3d_process(
-                    mining_obj.obj.as_slice(),
-                    algo.as_p3d_algo(),
-                    grid as i16,
-                    sect as i16,
-                    rot,
-                );
-
-                let (first_hash, obj_hash, poscan_hash) = match res_hashes {
-                    Ok(hashes) if !hashes.is_empty() => {
-                        let first_hash = hashes[0].clone();
-                        let obj_hash = H256::from_str(&first_hash).unwrap();
-                        if processed_hashes.contains(&obj_hash) {
-                            continue;
-                        }
-                        let poscan_hash = DoubleHash {
-                            pre_hash: prop.params.pre_hash.clone(),
-                            obj_hash,
-                        }
-                        .calc_hash();
-                        processed_hashes.insert(obj_hash.clone());
-                        (first_hash, obj_hash, poscan_hash)
-                    }
-                    _ => {
-                        continue;
-                    }
-                };
-
-                for difficulty in [
-                    prop.params.pow_difficulty.clone(),
-                    prop.params.win_difficulty.clone(),
-                ] {
-                    let comp = Compute {
-                        difficulty,
-                        pre_hash: prop.params.pre_hash.clone(),
-                        poscan_hash,
-                    };
-
-                    let diff = get_hash_difficulty(&comp.get_work());
-
-                    if diff >= difficulty {
-                        let prop = MiningProposal {
-                            params: prop.params.clone(),
-                            hash: obj_hash,
-                            obj_id: mining_obj.obj_id,
-                            obj: mining_obj.obj.clone(),
-                        };
-                        println!("obj_hash: {:?}", obj_hash);
-
-                        let res = ctx.push_to_pool(prop).await;
-                        if let Err(e) = res {
-                            println!("🟥 Error: {}", &e);
-                        }
-                    }
-                }
-            }
-        } else {
-            tokio::time::sleep(Duration::from_millis(100)).await;
-        }
-    }
-}
 
 pub(crate) async fn run_rpc_server(ctx: Arc<PoolContex>) -> anyhow::Result<SocketAddr> {
     let cors = CorsLayer::new()
@@ -155,17 +23,7 @@ pub(crate) async fn run_rpc_server(ctx: Arc<PoolContex>) -> anyhow::Result<Socke
 
     let mut module = RpcModule::new(ctx.clone());
 
-    module.merge(
-        MiningRpcServerImpl::new(
-            ctx.p3d_params.clone(),
-            ctx.pool_id.clone(),
-            ctx.member_id.clone(),
-            "Grid2dV3.1".into(),
-            ctx.key.clone(),
-            Arc::new(ctx.client.clone()),
-        )
-        .into_rpc(),
-    )?;
+    module.merge(MiningRpcServerImpl::new(ctx.clone()).into_rpc())?;
 
     let addr = server.local_addr()?;
     let handle = server.start(module);
@@ -173,19 +31,4 @@ pub(crate) async fn run_rpc_server(ctx: Arc<PoolContex>) -> anyhow::Result<Socke
     tokio::spawn(handle.stopped());
 
     Ok(addr)
-}
-
-pub(crate) fn start_timer(ctx: Arc<PoolContex>) {
-    let _forever = tokio::spawn(async move {
-        let mut interval = time::interval(ASK_MINING_PARAMS_PERIOD);
-
-        loop {
-            interval.tick().await;
-
-            let res = ctx.get_mining_params(ctx.pool_id.clone()).await;
-            if let Err(e) = res {
-                println!("🟥 Ask for mining params error: {}", &e);
-            }
-        }
-    });
 }
